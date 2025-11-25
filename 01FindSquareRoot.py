54cbb0d80adf8ea27c8e475963e2a1a6ef5291eb
Aya <kongaaaya@icloud.com>
2025-11-25 16:17:19 +0300

    Update 01FindSquareRoot.py

--- 01FindSquareRoot.py
+++ 01FindSquareRoot.py
@@ -1,10 +1,10 @@
 def binSearchSqrt(target, A):
     l = 0
-    r = len(A) - 1  # Fixed: use the last index of the array
+    r = len(A) - 1 
     
     while l <= r:
         middle = (l + r) // 2
-        if A[middle] ** 2 > target:  # Fixed: compare with target, not A[target]
+        if A[middle] ** 2 > target:
             r = middle - 1
         elif A[middle] ** 2 < target:
             l = middle + 1
